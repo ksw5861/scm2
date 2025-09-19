72009be4f3cba3c5d35013723e758d34a2f81b3d
mulumpyo <1119_dh@naver.com>
2025-09-19 16:19:14 +0900

    DB 설정

--- client/src/config/menus/menu/master.js
+++ client/src/config/menus/menu/master.js
@@ -1,13 +1,13 @@
 import warehouse from './dh/warehouse';
 import material from './sw/matetial';
 
-const menuTest =
-  {
-    label: '기준정보',
-    items: [
-        warehouse,
-        material
-    ]
-  };
+const master =
+    {
+        label: '기준정보',
+        items: [
+            warehouse,
+            material
+        ]
+    };
 
-export default menuTest;
+export default master;
--- server/scm/src/main/java/com/yedam/scm/web/DhController.java
+++ server/scm/src/main/java/com/yedam/scm/web/DhController.java
@@ -9,11 +9,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
-public class WarehouseController {
+public class DhController {
 
   @Autowired
   WarehouseService svc;
@@ -22,5 +20,5 @@ public class WarehouseController {
   public List<WarehouseVO> getMethodName() {
       return svc.getWarehouseList();
   }
-  
+
 }
