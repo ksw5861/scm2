a975b1e94154bdc30458a8e2e36d317d34f7c4eb
mulumpyo <1119_dh@naver.com>
2025-09-27 22:36:53 +0900

    김대환/로그인-JWT_구현_완료/250927

--- server/scm/src/main/java/com/yedam/scm/login/service/impl/LoginServiceImpl.java
+++ server/scm/src/main/java/com/yedam/scm/login/service/impl/LoginServiceImpl.java
@@ -7,6 +7,7 @@ import com.yedam.scm.dto.LoginDTO;
 import com.yedam.scm.dto.LoginRes;
 import com.yedam.scm.login.mapper.LoginMapper;
 import com.yedam.scm.login.service.LoginService;
+import com.yedam.scm.security.JwtUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,16 +16,17 @@ import lombok.RequiredArgsConstructor;
 public class LoginServiceImpl implements LoginService {
 
     private final LoginMapper mapper;
+    private final JwtUtil jwtUtil;
 
     @Override
     public LoginRes loginByEmailAndPassword(LoginDTO login) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         LoginRes res = mapper.selectAccountByEmailAndPassword(login);
-        
+
         boolean isMatch = encoder.matches(login.getPassword(), res.getPasswordHash());
 
-        if(isMatch) {
-            res.setPasswordHash(null);
+        if (isMatch) {
+            res.setAccessToken(jwtUtil.generateToken(res));
             return res;
         } else {
             return null;
