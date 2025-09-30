5a9492b7cefdb128282379d9588fd6d6dcc58887
kdw <tunaya0325@gmail.com>
2025-10-01 03:06:54 +0900

    김동원/반품 목록

--- server/scm/src/main/java/com/yedam/scm/vo/ReturnVO.java
+++ server/scm/src/main/java/com/yedam/scm/vo/ReturnVO.java
@@ -7,12 +7,13 @@ import lombok.Data;
 
 @Data
 public class ReturnVO {
-    private String returnId;       // 반품번호
-    private String returnDate;       // 반품일자
-    private String vendorId;       // 거래처 ID
-    private Long returnPrice;      // 반품 총액
-    private String returnStatus;         // 반품 상태 (예: 반품접수, 반품완료)
-    
+    private String returnId; // 반품번호
+    // private String returnDate; // 반품일자
+    private Date returnDate; // 반품일자
+    private String vendorId; // 거래처 ID
+    private Long returnPrice; // 반품 총액
+    private String returnStatus; // 반품 상태 (예: 반품접수, 반품완료)
+
     // =========== 주문(반품) 조회용 필드 ===========
     private String orderId;
     private Date orderDate;
@@ -21,13 +22,10 @@ public class ReturnVO {
     private String prodName; // 대표 상품명 (필요 시)
 
     // =========== 상세 리스트 ===========
-    private List<ReturnDetailVO> details;  // 반품 상세 정보
-
+    private List<ReturnDetailVO> details; // 반품 상세 정보
 
     // 0930 동원 추가
 
     private String companyName; // 판매처명
 
-
 }
-
