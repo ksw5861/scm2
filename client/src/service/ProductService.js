bd208032a88b4db9081134d3e9bba41c5b5f16ac
mulumpyo <145254729+mulumpyo@users.noreply.github.com>
2025-10-23 17:15:51 +0900

    Update ProductService.js

--- client/src/service/ProductService.js
+++ client/src/service/ProductService.js
@@ -2,18 +2,6 @@ export const ProductService = {
     getProductsData() {
         return [
             {
-                id: '1000',
-                code: 'f230fh0g3',
-                name: 'Bamboo Watch',
-                description: 'Product Description',
-                image: 'bamboo-watch.jpg',
-                price: 65,
-                category: 'Accessories',
-                quantity: 24,
-                inventoryStatus: 'INSTOCK',
-                rating: 5
-            },
-            {
                 id: '1001',
                 code: 'nvklal433',
                 name: 'Black Watch',
