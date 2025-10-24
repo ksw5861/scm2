c519c1464859e6bb90911cf5619c424f305715d3
Sangwoo, Kim <kimsw5861@naver.com>
2025-10-24 12:48:54 +0900

    20251024

--- server/scm/src/main/java/com/yedam/scm/vo/BomVO.java
+++ server/scm/src/main/java/com/yedam/scm/vo/BomVO.java
@@ -3,7 +3,6 @@ package com.yedam.scm.vo;
 import java.util.Date;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
@@ -11,23 +10,16 @@ import lombok.Data;
 public class BomVO {
     private String bomId;
     private String bomVersion;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private Date effectiveDate;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private Date expireDate;
     private String prodId;
-    private String matId;
     private Date createdAt;
-    private Integer qty; // 수량
+    private Double lossRate;
     private Date lastUpdateDate;
 
-    private Double lossRate;
-
-    private MaterialVO material; // 자재 정보 포함
+    // 조인용
     private ProductVO product;
-    private BomDetailVO bomDetail;
-    
-
-private List<BomDetailVO> details; // BOM_DETAIL 리스트
-
+    private List<BomDetailVO> details;
 }
+
+
