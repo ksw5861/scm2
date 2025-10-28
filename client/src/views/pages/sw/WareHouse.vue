3ce1b242d5800fa871aae91cdcfc7b7325bafee6
Sangwoo, Kim <kimsw5861@naver.com>
2025-10-28 09:20:50 +0900

    ksw/20251027

--- client/src/views/pages/sw/WareHouse.vue
+++ client/src/views/pages/sw/WareHouse.vue
@@ -51,7 +51,7 @@ const searchWhSuggestions = async (event) => {
   }
 
   try {
-    const { data } = await axios.get(`http://13.124.12.49/api/warehouse1/autocomplete?keyword=${keyword}`);
+    const { data } = await axios.get(`http://43.201.78.169/api/warehouse1/autocomplete?keyword=${keyword}`);
     suggestions.value = data;
   } catch {
     suggestions.value = [];
