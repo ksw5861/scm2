d1a3dd86ae3895fa8efb69bab020fb7ffed41229
min-seo7 <mms1117777@gmail.com>
2025-10-28 22:18:11 +0900

    min/251028 22:18

--- client/src/config/menus/menu/ms/purchaseOrder.js
+++ client/src/config/menus/menu/ms/purchaseOrder.js
@@ -1,7 +1,7 @@
 import { useIcon } from "@/composables/useIcon";
 
 const purchaseOrder = {
-  label: '자재 발주 내역 조회',
+  label: '자재 발주 조회',
   icon: useIcon('list'),
   to: '/purchaseOrder',
   roles: ['admin', 'employee']
